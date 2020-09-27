42787f44e03435fd9ee4d536e836fe7fd427e873
ma-ri0328 <68724951+ma-ri0328@users.noreply.github.com>
2020-09-27 14:48:46 +0900

    create-new-posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show]
   def index
     @posts = Post.all
   end
@@ -11,8 +12,28 @@ class PostsController < ApplicationController
     Post.create(post_params)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+  end
+
   private
   def post_params
     params.require(:post).permit(:name, :image, :text)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
